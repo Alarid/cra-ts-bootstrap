92dcf4ab8c202c23d0d07e6eb53163ed552249e6
Yohann Legrand <yoh.legrand@gmail.com>
2021-02-05 18:34:38 +0100

    feat(global): first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+
+const App: React.FC = () => {
+  return (
+    <Container>
+      <Jumbotron className="mt-5">
+        <h1>
+          CRA TypeScript Bootstrap Template{' '}
+          <span role="img" aria-label="rocket emoji">
+            🚀
+          </span>
+        </h1>
+        <p>Ready to roll !</p>
+      </Jumbotron>
+    </Container>
+  )
+}
+
+export default App
